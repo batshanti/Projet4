5029d24a61525666664092a1b2054125723f82f4
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2021-12-30 16:31:27 +0100

    Flake8 > tournament_controller.py

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -29,7 +29,6 @@ class Tournament_controller:
             liste_p.append(player)
         return liste_p
 
-
     def first(self):
 
         self.play_tournament_view.players_tournament(self.tournament.sort_player_rank())
@@ -37,7 +36,11 @@ class Tournament_controller:
         round_1_score = self.play_tournament_view.first_round(self.tournament.players_object, self.tournament.name)
         round_1 = Round("Round_1", start_time)
         self.tournament.rounds = [round_1.save(round_1_score)]
-        self.tournament.players_object =  Tournament_controller.update_score(self.tournament.players_object, round_1_score)
+        self.tournament.players_object = Tournament_controller.update_score(
+            self.tournament.players_object,
+            round_1_score
+            )
+
         self.tournament.save_tournament(1)
         next = -1
         while next != 0 and next != 1:
@@ -48,41 +51,49 @@ class Tournament_controller:
 
         round_number = self.tournament.check_round() + 1
         round_name = "Round_"+str(round_number)
-        # sort_score_players = sorted(self.tournament.players, key=lambda t: (t['score'],t['rank']), reverse=True)
         start_time = time.strftime("%d/%m/%Y")+" "+time.strftime("%Hh%Mm%Ss")
         self.tournament.sort_player_score()
-        round_score = self.play_tournament_view.round(self.tournament.players_object, self.tournament.name, round_name)    
+        round_score = self.play_tournament_view.round(
+            self.tournament.players_object,
+            self.tournament.name, round_name
+            )
+
         round = Round(round_name, start_time)
         self.tournament.rounds = self.tournament.rounds + [round.save(round_score)]
-        self.tournament.players_object =  Tournament_controller.update_score(self.tournament.players_object, round_score)
+        self.tournament.players_object = Tournament_controller.update_score(
+            self.tournament.players_object,
+            round_score
+            )
+
         self.tournament.save_tournament(1)
         next = -1
         while next != 0 and next != 1:
             next = self.play_tournament_view.next_round()
         return next
 
-
     def check_players(self):
         if len(self.tournament.players) == 0:
             return False
         else:
             return True
-            
-    @staticmethod    
+
+    @staticmethod
     def update_score(players, score):
-        tab_score = [[score[0][0], score[0][1]],
-                    [score[0][2], score[0][3]],
-                    [score[1][0], score[1][1]],
-                    [score[1][2], score[1][3]],
-                    [score[2][0], score[2][1]],
-                    [score[2][2], score[2][3]],
-                    [score[3][0], score[3][1]],
-                    [score[3][2], score[3][3]]]   
+        tab_score = [
+            [score[0][0], score[0][1]],
+            [score[0][2], score[0][3]],
+            [score[1][0], score[1][1]],
+            [score[1][2], score[1][3]],
+            [score[2][0], score[2][1]],
+            [score[2][2], score[2][3]],
+            [score[3][0], score[3][1]],
+            [score[3][2], score[3][3]]
+            ]
 
         for player in players:
             for line in tab_score:
                 if player.identity == line[0]:
                     player.score = (float(player.score) + float(line[1]))
-                
+
         return players
 
